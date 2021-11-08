ed3edab7c5aa3bc56151dca92e7bf2b4059b99fb
Rafael Chaidez <rafael.chaidez.r@gmail.com>
2021-11-08 00:16:53 -0700

    se agrego funcion dados

--- juegos.py
+++ juegos.py
@@ -14,6 +14,17 @@ def tombola():
         else:
             print('Sigue paticipando')
 
+def dados():
+    dado1 = random.randint(1,6)
+    dado2 = random.randint(1,6)
+    print("Primer dado: ",dado1)
+    print("Segundo dado: ",dado2)
+    suma = dado1 + dado2
+    if suma == 7:
+        print("!Ganaste!")
+    else:
+        print("Mejor suerte para la proxima")
+
 # Menu de juego
 def menu():
     i = 0
