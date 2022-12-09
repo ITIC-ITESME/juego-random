32293ce5754f4e1a807fed5a583f61eced69b3fb
Eduardo <l181061139@mulege.tecnm.mx>
2022-12-09 16:12:37 -0700

    Se agrego juego dados a funcion menu

--- juegos.py
+++ juegos.py
@@ -13,19 +13,6 @@ def dados():
     else:
         print("Mejor suerte para la proxima")
 
-def tombola():
-    oportunidades = 10 
-    for i in range(oportunidades):
-        numeroSuerte = int(input('Escribe un numero del 1 al 10: '))
-            # randint([inicio],[final])
-        tombola = random.randint(1,10)
-        print('El numero es ', tombola)
-        if numeroSuerte==tombola:
-            print('!Ganaste!')
-            break
-        else:
-            print('Sigue paticipando')
-
 # Menu de juego
 def menu():
     i = 0
@@ -40,29 +27,12 @@ def menu():
             tombola()
         elif i == 2:
             print("\nDados")
- adriancarreto
-            dados_adriancarreto()
-
-            dados_JIR()
-main
+            dados()
         elif i == 3:
             break
         else:
             print("Opcion invalida")
-adriancarreto
-            
-#Los datos al azar
-def dados_adriancarreto():
-    dato1 = random.randint(1,6)
-Modifiquen la función por su usuario, donde se declara y cuando se llama.
-
-def dados_adriancarreto():
-Dentro de menu()
-
-elif i == 2:
-            print("\nDados")
-            dados_adriancarreto()
 
 if __name__ == "__main__":
     menu()
- main
+ 
